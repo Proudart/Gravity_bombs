b9082f2924497334d82f94478e005c14342507f6
Kahuds <deividaspa@gmail.com>
2021-11-10 16:44:33 +0000

    Co-authored-by: LtJauman <LtJauman@users.noreply.github.com>

--- Gravity_bomb/script.js
+++ Gravity_bomb/script.js
@@ -12,6 +12,10 @@ var wall2;
 var tank1;
 var tank2;
 var fuzzball;
+var mode = 0;
+
+
+
 
 
 class c_ground {
@@ -47,7 +51,7 @@ class c_ground {
 
 
 class tankCreate {
-    constructor(x, y, width, height, colour) {
+    constructor(x, y, width, height, colour,state) {
         let options = {
                 restitution: 0.99,
                 friction: 0.030,
@@ -57,12 +61,12 @@ class tankCreate {
             //create the body
         this.body = Matter.Bodies.rectangle(x, y, width, height, options);
         Matter.World.add(world, this.body); //add to the matter world
-
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
         this.colour = colour;
+        this.state = state
     }
 
 
@@ -70,8 +74,7 @@ class tankCreate {
         return this.body; //return the created body
     }
     show() {
-        let pos = this.body.position; //create an shortcut alias 
-        console.log(pos.x)
+        let pos = this.body.position; //create an shortcut alias
         let angle = this.body.angle;
         push(); //p5 translation 
         stroke("#000000");
@@ -81,12 +84,43 @@ class tankCreate {
         rotate(angle);
         rect(0, 0, this.width, this.height);
         pop();
+    }
 
-        function barrelAim() {
-            rect(0, 0, this.width, this.height);
-        }
+    barrelAim() {
+        let pos = this.body.position;
+        switch(this.state) {
+            case true:push(); translate(pos.x,pos.y-20); let a = atan2(mouseY - pos.y,mouseX - pos.x); rotate(a); rect(0, 0, 60, 20); pop(); break;
+            case false: push(); translate(pos.x,pos.y-20); rect(0, 0, 60, 20); pop(); break;
     }
 }
+}
+
+// if (keyIsPressed) {
+//     if (keyCode == RIGHT_ARROW) {
+//      circleX +=5; 
+//     }  else if (keyCode == LEFT_ARROW) {
+//       circleX -= 5;
+//     } else if (keyCode == UP_ARROW) {
+//       circleY -= 5; 
+//     } else if (keyCode == DOWN_ARROW) {
+//       circleY +=5; 
+//     }
+//    }  
+
+
+function mouseClicked() {
+    if (tank1['state'] == true) {tank1['state'] = false; } else tank1['state'] = true
+    if (tank2['state'] == true) {tank2['state'] = false} else tank2['state'] = true
+}
+
+
+function keyIsDown() { 
+    switch (keyCode) {
+		//case UP_ARROW: Matter.Body.setVelocity(fuzzball.body, { x: mouseX/50, y: mouseY/50 }); break;
+		case UP_ARROW: Matter.Body.setVelocity(tank1.body, { x: 5, y: 0 }); break;
+	}
+}
+
 
 
 class c_fuzzball {
@@ -125,13 +159,6 @@ class c_fuzzball {
 
 //test for mouse position
 
-function keyPressed() { 
-    switch (keyCode) {
-		case UP_ARROW: Matter.Body.setVelocity(fuzzball.body, { x: mouseX/50, y: mouseY/50 }); break;
-	}
-}
-
-
 
 function apply_angularvelocity() {
     Matter.Body.setAngularVelocity(tank1.body, Math.PI / get_random(3, 20));
@@ -165,13 +192,15 @@ function initialiseGame() {
     ground = new c_ground(vp_width / 2, vp_height - 10, vp_width, 20);
     wall1 = new c_ground(vp_width, vp_height / 2, 20, vp_height);
     wall2 = new c_ground(0, vp_height / 2, 20, vp_height);
-    tank1 = new tankCreate(get_random(0, (vp_width / 2)), 600, 80, 40, "#708090");
-    tank2 = new tankCreate(get_random((vp_width / 2), vp_width), 600, 80, 40, "#00ff7f");
+    tank1 = new tankCreate(get_random(0, (vp_width / 2)), 600, 80, 40, "#708090",false);
+    tank2 = new tankCreate(get_random((vp_width / 2), vp_width), 600, 80, 40, "#00ff7f",true);
     fuzzball = new c_fuzzball(400, 200, 60);
 }
 
 
-function setup() {
+function setup() { // Menu should go here 
+    let mode = 0;
+    textSize(21);
     //this p5 defined function runs automatically once the preload function is done
     viewport = createCanvas(vp_width, vp_height); //set the viewport (canvas) size
     viewport.parent("viewport_container"); //move the canvas so it’s inside the target div
@@ -197,16 +226,22 @@ function paint_assets() {
     ground.show();
     wall1.show();
     wall2.show();
+    tank1.barrelAim();
     tank1.show();
+    tank2.barrelAim();
     tank2.show();
     fuzzball.show();
 }
 
 
 function draw() {
+
     //this p5 defined function runs every refresh cycle
     Matter.Engine.update(engine); //run the matter engine update
 
     paint_background(); //paint the default background
     paint_assets(); //paint the assets
+    if (mode == 0){
+        text("Press enter to start", 20, 40);
+    }
 }
